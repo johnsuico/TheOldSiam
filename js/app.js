02be0b761e434dcc5d7f1f2e4d0ee99d6c0832cb
John Suico <johncdsuico@gmail.com>
2020-05-26 08:59:15 -0700

    created slide menu

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const navSlide = () => {
+    const menu = document.querySelector('.nav-link');
+    const menuLinks = document.querySelector('.menu-links');
+
+    // Toggle Nav
+    menu.addEventListener('click', () => {
+        menuLinks.classList.toggle('nav-active');
+    });
+}
+
+navSlide();
