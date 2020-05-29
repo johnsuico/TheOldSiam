9723dbdcc1f946ca1f345b8fdb80ed61ae014385
John Suico <johncdsuico@gmail.com>
2020-05-28 18:21:05 -0700

    trial 14

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-// const navSlide = () => {
-//     const menu = document.querySelector('.nav-link');
-//     const menuLinks = document.querySelector('.menu-links');
+const navSlide = () => {
+    const menu = document.querySelector('.nav-link');
+    const menuLinks = document.querySelector('.menu-links');
 
-//     //Toggle Nav
-//     menu.addEventListener('click', () => {
-//         menuLinks.classList.toggle('nav-active');
-//     });
-// }
+    //Toggle Nav
+    menu.addEventListener('click', () => {
+        menuLinks.classList.toggle('nav-active');
+    });
+}
 
 // const navSlide = function() {
 //     const menu = document.querySelector('.nav-link');
@@ -18,31 +18,31 @@
 //     });
 // }
 
-// navSlide();
+navSlide();
 
-const navSlide = () => {
-    const menu = document.querySelector('.nav-link');
-    const nav = document.querySelector('.menu-links');
-    // const html = document.querySelector('html');
-    const navLinks = document.querySelectorAll('.menu-links li');
+// const navSlide = () => {
+//     const menu = document.querySelector('.nav-link');
+//     const nav = document.querySelector('.menu-links');
+//     // const html = document.querySelector('html');
+//     const navLinks = document.querySelectorAll('.menu-links li');
 
-    // Toggle Nav
-    menu.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        // html.classList.toggle('no-scroll');
+//     // Toggle Nav
+//     menu.addEventListener('click', () => {
+//         nav.classList.toggle('nav-active');
+//         // html.classList.toggle('no-scroll');
 
-        // Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
-        //Burger animation
-        burger.classList.toggle('toggle');
+//         // Animate Links
+//         navLinks.forEach((link, index) => {
+//             if (link.style.animation) {
+//                 link.style.animation = '';
+//             } else {
+//                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+//             }
+//         });
+//         //Burger animation
+//         burger.classList.toggle('toggle');
 
-    });
-}
+//     });
+// }
 
 navSlide();
