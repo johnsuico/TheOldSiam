40a8913dec24fc2b0956cc6f4c5a3c001126668b
John Suico <johncdsuico@gmail.com>
2020-05-28 17:10:33 -0700

    fixed mobile menu

--- js/app.js
+++ js/app.js
@@ -1,16 +1,10 @@
-// const 
-var navSlide = () => {
-    // const menu = document.querySelector('.nav-link');
-    var menu = document.querySelector('.nav-link');
-    // const menuLinks = document.querySelector('.menu-links');
-    var menuLinks = document.querySelector('.menu-links');
+const navSlide = () => {
+    const menu = document.querySelector('.nav-link');
+    const menuLinks = document.querySelector('.menu-links');
 
     // Toggle Nav
     menu.addEventListener('click', () => {
         menuLinks.classList.toggle('nav-active');
-    });
-    menu.addEventListener('touchend', () => {
-        menuLinks.classList.toggle('nav-active'); 
     });
 }
 
