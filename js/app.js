94a86f0cfa72cad717811c1fd46ed2e72a41505f
John Suico <johncdsuico@gmail.com>
2020-05-28 19:42:23 -0700

    trial 21

--- js/app.js
+++ js/app.js
@@ -1,37 +1,10 @@
-// const navSlide = () => {
-//     const menu = document.querySelector('.nav-link');
-//     const menuLinks = document.querySelector('.menu-links');
-
-//     //Toggle Nav
-//     menu.addEventListener('click', () => {
-//         menuLinks.classList.toggle('nav-active');
-//     });
-// }
-
-// navSlide();
-
 const navSlide = () => {
-    const menu = document.querySelector('.nav-link');
-    const nav = document.querySelector('.menu-links');
-    const html = document.querySelector('html');
-    const navLinks = document.querySelectorAll('.nav-links li');
+    const menu = document.querySelector('.menu');
+    const menuLinks = document.querySelector('.menu-links');
 
     // Toggle Nav
     menu.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        html.classList.toggle('no-scroll');
-
-        // Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
-        //Burger animation
-        burger.classList.toggle('toggle');
-
+        menuLinks.classList.toggle('nav-active');
     });
 }
 
