b38329f6b178d293f494ad8ad095bfb6bf2e225b
John Suico <johncdsuico@gmail.com>
2020-05-28 17:30:13 -0700

    trial 4

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
-const navSlide = () => {
+// const navSlide = () => {
+//     const menu = document.querySelector('.nav-link');
+//     const menuLinks = document.querySelector('.menu-links');
+
+//     // Toggle Nav
+//     menu.addEventListener('click', () => {
+//         menuLinks.classList.toggle('nav-active');
+//     });
+// }
+
+const navSlide = function() {
     const menu = document.querySelector('.nav-link');
     const menuLinks = document.querySelector('.menu-links');
 
