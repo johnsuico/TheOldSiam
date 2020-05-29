0bddd22112e8397fe5257132d4babc26902a3c24
John Suico <johncdsuico@gmail.com>
2020-05-28 18:22:24 -0700

    trial 15

--- js/app.js
+++ js/app.js
@@ -8,17 +8,7 @@ const navSlide = () => {
     });
 }
 
-// const navSlide = function() {
-//     const menu = document.querySelector('.nav-link');
-//     const menuLinks = document.querySelector('.menu-links');
-
-//     // Toggle Nav
-//     menu.addEventListener('click', () => {
-//         menuLinks.classList.toggle('nav-active');
-//     });
-// }
-
-navSlide();
+// navSlide();
 
 // const navSlide = () => {
 //     const menu = document.querySelector('.nav-link');
